17b73f26350c9f79e0e4194fedc614a0c564ff84
rachelsison <rsison87@gmail.com>
2014-11-07 20:50:14 -0800

    Have draggable object

--- watchout.js
+++ watchout.js
@@ -1,28 +1,21 @@
-// start slingin' some d3 here.
-// var gameOptions = {
-//   height: 450,
-//   width: 700,
-//   nEnemies: 30,
-//   padding: 20
-// }
 
-// var gameStats = {
-//   score: 0,
-//   bestScore: 0
-// }
-
-// var axes = {
-//   x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
-//   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
-// }
-
-// var gameBoard = function(){
-//   d3.select('.container').append('svg:svg')
-//     .attr('width', gameOptions.width)
-//     .attr('height', gameOptions.height)
-//   }
+var drag = d3.behavior.drag()
+  .on('dragstart', function(){ console.log('started dragging');})
+  .on('drag', function(){ d3.select('ellipse').attr('cx', d3.event.x)
+                                  .attr('cy', d3.event.y); })
+  .on('dragend', function(){ console.log('stopped dragging');});
 
 var svg = d3.select("body svg");
+
+svg.append("ellipse")
+.data([{x:300, y:200}])
+.attr("cx", function(d) {return d.x})
+.attr("cy", function(d) {return d.y})
+.attr("rx", 20)
+.attr("ry", 15)
+.style("fill", "red")
+.call(drag)
+;
 
 var rand = function(){
   return Math.floor(Math.random() * 500 );
@@ -64,53 +57,4 @@ var changeLocation = function(){
 
 setInterval(changeLocation, 3000);
 
-// var svg = d3.select("body").append("svg")
-//     .attr("width", 100)
-//     .attr("height", 100)
-//   .append("g")
-    //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// $(body).append("span.container");
-
-// d3.select(".container1").selectAll("div")
-//   .data(['10px', '30px', '15px', '6px', '8px','17px'])
-//   .enter()
-//   .append("div")
-//   .style('top', function(d){return d;});
-
-
-
-
-
- /*d3.select('.container').selectAll('div')
-  .data(['10px', '30px', '15px', '6px', '8px'])
-  //.enter()
- .style('left', function(d){return d;}).addClass("circle");
-
-
-
-
-
-d3.select('.container').selectAll('div')
-  .data(['10px', '30px', '15px', '6px', '8px'])
-  //.enter()
-  .style('height', function(d){return d;});
-*/
-
-
-// var testThis = d3.selectAll("svg")
-//   .data(["red","black","yellow"])
-//   .style("background-color",function(d) {return d;} );
-
-// svg.on("mousemove", function() {
-//   var p1 = d3.mouse(this);
-//   //reset score to 0
-//   console.log('mousedThis');
-// });
-
-//create a board
-//populate the board with svg objects that are the same and randomly placed
-
-
-//update method is going to change the cx and cy of svg objects to random loc
 
