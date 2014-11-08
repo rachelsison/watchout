c71cf91d33c08007e8131c0377001fff1be52912
rachelsison <rsison87@gmail.com>
2014-11-08 12:13:54 -0800

    Collisions partially working, score working

--- watchout.js
+++ watchout.js
@@ -1,3 +1,7 @@
+var highScore = 0;
+var currentScore = 0;
+var collisions = 0;
+
 
 var drag = d3.behavior.drag()
   .on('dragstart', function(){ console.log('started dragging');})
@@ -16,6 +20,9 @@ svg.append("ellipse")
 .style("fill", "red")
 .call(drag)
 ;
+
+var player = d3.select("body svg ellipse");
+//console.log(player.attr('cx'));
 
 var rand = function(){
   return Math.floor(Math.random() * 500 );
@@ -43,18 +50,83 @@ var randArr = function (){
 
 populateBoard();
 
+var resetScore = function(){
+  if(currentScore > highScore){
+    highScore = currentScore;
+  }
+  collisions += 1;
+  currentScore = 0;
+  d3.select('.high span').text(highScore);
+  d3.select('.current span').text(0);
+  d3.select('.collisions span').text(collisions);
+
+}
+
+var increaseScore = function () {
+  currentScore++;
+  d3.select('.current span').text(currentScore);
+}
+
+var checkCollision = function(circle){
+  var radiusSum = parseFloat(circle.attr('r')) + parseFloat(player.attr('ry'));
+  var xDiff = parseFloat(circle.attr('cx')) - parseFloat(player.attr('cx'));
+  var yDiff = parseFloat(circle.attr('cy')) - parseFloat(player.attr('cy'));
+
+  var distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow( yDiff, 2))
+  if(distance < radiusSum){
+    resetScore();
+  }
+}
+
+var ourTween = function(locations, self){
+
+  var enemy=d3.select(self);
+  // console.log('locations:');
+  // console.log(locations);
+  // console.log('enemy:');
+  // console.log(enemy);
+  // console.log('enemy.attr("cx")');
+  // console.log(enemy.attr("cx") + 10000);
+  var currPos = {x:parseFloat(enemy.attr("cx")), y: parseFloat(enemy.attr("cy"))}
+  var endPos = {x:locations.cx,y:locations.cy};
+  return function(t) {
+    console.log('running function')
+    checkCollision(enemy);
+    var nextPos = {x: currPos.x+(endPos.x-currPos.x)*t,
+               y: currPos.y+(endPos.y-currPos.y)*t };
+    enemy.attr('cx',nextPos.x)
+         .attr('cy',nextPos.y);
+  }
+}
+
+
+
 var changeLocation = function(){
   var locations = [];
   for (var i=0; i<20; i++) {
     locations.push({"cx":rand(),"cy":rand()})
   }
-  svg.selectAll("circle").data(locations).transition().duration(1900).attr("cx", function(d) {
-    return d.cx;
-  }).attr("cy", function(d) {
-    return d.cy;
+  svg.selectAll("circle")
+  .data(locations)
+  .transition()
+  .duration(1900)
+  .tween('custom', function(d){
+    var self = this;
+    return ourTween(d, self);
   })
+    .attr("cx", function(d) {
+    // console.log("THIS ATTR:")
+    // console.log(this.;
+
+    return d.cx;
+    })
+    .attr("cy", function(d) {
+    return d.cy;
+    })
+
 }
 
 setInterval(changeLocation, 3000);
+setInterval(increaseScore,50);
 
 
